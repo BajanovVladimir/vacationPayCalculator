4b7ee57cadb5e884e890eb31bcc1c4508d8b962d
BajanovVladimir <bajanov_v_s@mail.ru>
2024-09-11 17:15:17 +0300

    Added  test for calculationViewController

---
+++ src/test/java/ru/bazhanov/web/CalculateViewControllerTests.java
@@ -0,0 +1,37 @@
+package ru.bazhanov.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class CalculateViewControllerTests {
+
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+    }
+
+    @Test
+    void test_that_calculateView_is_ok() throws Exception {
+        String url = "/calculate";
+        this.mockMvc.perform(get(url))
+                .andExpect(status().isOk());
+    }
+
+}
