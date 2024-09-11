c8f7b8da3e02aff2725857646d232abf35067e23
BajanovVladimir <bajanov_v_s@mail.ru>
2024-09-11 16:10:20 +0300

    Initial commit. Project setup

---
+++ src/main/java/ru/bazhanov/Application.java
@@ -0,0 +1,11 @@
+package ru.bazhanov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(ru.bazhanov.Application.class,args);
+    }
+}
